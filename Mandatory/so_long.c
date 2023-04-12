6c3f387b871cdb8935f0ccfa23e915845e4a40e9
El Mehdi Bennix <ebennix@e1r10p14.1337.ma>
2023-04-12 04:20:24 +0000

    MakefileAutoPush

--- Mandatory/so_long.c
+++ Mandatory/so_long.c
@@ -6,11 +6,27 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 06:21:19 by ebennix           #+#    #+#             */
-/*   Updated: 2023/04/12 03:19:24 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/04/12 04:11:31 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
+
+typedef struct	s_tst {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_tst;
+
+void	my_mlx_pixel_put(t_tst *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
 
 void so_long(char **av)
 {
@@ -18,11 +34,18 @@ void so_long(char **av)
 
     map = (t_data *)ft_calloc(sizeof(t_data),1);
     read_map(*(++av), map);
+    
 	void	*mlx;
 	void	*mlx_win;
+	t_tst	img;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	my_mlx_pixel_put(&img, 500, 100, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
 
