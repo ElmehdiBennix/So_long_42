c433fb1f5b47f611c2e08807e5a65cac1416c228
El Mehdi Bennix <ebennix@e1r8p12.1337.ma>
2023-04-23 20:46:33 +0100

    MakefileAutoPush

--- utils/libft/string/ft_strchr.c
+++ utils/libft/string/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 02:47:34 by ebennix           #+#    #+#             */
-/*   Updated: 2023/04/20 02:43:37 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/04/23 19:44:38 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strchr(const char *s, int c )
 	int		i;
 
 	i = 0;
+	if (!s)
+		return (NULL);
 	while (s[i] != '\0')
 	{
 		if (s[i] == (char)c)
