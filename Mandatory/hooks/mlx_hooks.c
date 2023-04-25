cc3814f43986e77f2b9ebc5c3e899dcf0ffa2137
El Mehdi Bennix <ebennix@e1r8p14.1337.ma>
2023-04-25 01:33:48 +0100

    MakefileAutoPush

--- Mandatory/hooks/mlx_hooks.c
+++ Mandatory/hooks/mlx_hooks.c
@@ -6,7 +6,7 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 07:09:42 by ebennix           #+#    #+#             */
-/*   Updated: 2023/04/25 00:30:25 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/04/25 00:33:45 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,24 @@
 
 static  void WSAD_keys(int key_code, t_data *game)
 {
-    printf("%d\n",key_code);
     if (key_code == mac_W || key_code == mac_UP)
     {
-        // mlx_destroy_image(game->mlx, game->componets.player);
+        mlx_destroy_image(game->mlx, game->componets.player);
         mlx_put_image_to_window(game->mlx ,game->mlx_window, game->componets.player , 96 * (game->p_position.y) , 96 * (game->p_position.x -= 1));
     }
     else if (key_code == mac_S || key_code == mac_DOWN)
     {
-        // mlx_destroy_image(game->mlx, game->componets.player);
+        mlx_destroy_image(game->mlx, game->componets.player);
         mlx_put_image_to_window(game->mlx ,game->mlx_window, game->componets.player , 96 * (game->p_position.y) , 96 * (game->p_position.x += 1));
     }
 	else if (key_code == mac_A || key_code == mac_LEFT)
     {
-        // mlx_destroy_image(game->mlx, game->componets.player);
+        mlx_destroy_image(game->mlx, game->componets.player);
         mlx_put_image_to_window(game->mlx ,game->mlx_window, game->componets.player , 96 * (game->p_position.y -= 1 ) , 96 * (game->p_position.x));
     }
     else if (key_code == mac_D || key_code == mac_RIGHT)
     {
-        // mlx_destroy_image(game->mlx, game->componets.player);
+        mlx_destroy_image(game->mlx, game->componets.player);
         mlx_put_image_to_window(game->mlx ,game->mlx_window, game->componets.player , 96 * (game->p_position.y += 1) , 96 * (game->p_position.x));
     }
 }
@@ -40,6 +39,7 @@ static  void WSAD_keys(int key_code, t_data *game)
 
 int    key_hooks(int key_code, t_data *game) // change struct name to game
 {
+    printf("%d\n",key_code);
     if (key_code == mac_W || key_code == mac_S || key_code == mac_A || key_code == mac_D)
         WSAD_keys(key_code, game);
     else if (key_code == mac_UP || key_code == mac_DOWN || key_code == mac_LEFT || key_code == mac_RIGHT)
