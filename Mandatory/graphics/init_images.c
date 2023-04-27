f4b4c8fc6f117429b81b3456e942f2aebf65a067
El Mehdi Bennix <ebennix@e1r8p12.1337.ma>
2023-04-27 10:04:06 +0100

    MakefileAutoPush

--- Mandatory/graphics/init_images.c
+++ Mandatory/graphics/init_images.c
@@ -6,7 +6,7 @@
 /*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 07:05:48 by ebennix           #+#    #+#             */
-/*   Updated: 2023/04/27 08:34:27 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/04/27 09:04:04 by ebennix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,27 +29,29 @@ static	void	init_walls(t_data *map_data)
 	map_data->walls.top_wall_v2 = mlx_xpm_file_to_image(map_data->mlx,"textures/wall/top_wall_v2.xpm",&map_data->walls.img_width,&map_data->walls.img_height);
 }
 
-static	void	init_floors(t_data *map_data)
-{
-	map_data->floors.floor = mlx_xpm_file_to_image(map_data->mlx,"textures/floor/floor.xpm",&map_data->floors.img_width,&map_data->floors.img_height);
-}
-
 static	void	init_componets(t_data *map_data)
 {
 	map_data->componets.player = mlx_xpm_file_to_image(map_data->mlx,"textures/player/player.xpm",&map_data->componets.img_width,&map_data->componets.img_height);
 	map_data->componets.exit = mlx_xpm_file_to_image(map_data->mlx,"textures/exit/exit.xpm",&map_data->componets.img_width,&map_data->componets.img_height);
 	map_data->componets.collectible = mlx_xpm_file_to_image(map_data->mlx,"textures/collectible/coin.xpm",&map_data->componets.img_width,&map_data->componets.img_height);
+	map_data->floors.floor = mlx_xpm_file_to_image(map_data->mlx,"textures/floor/floor.xpm",&map_data->floors.img_width,&map_data->floors.img_height);
+}
 
-	// map_data->floors.left_right_floor = mlx_xpm_file_to_image(map_data->mlx,"textures/floor/left_right_floor.xpm",&map_data->floors.img_width,&map_data->floors.img_height);
+static void protections (t_data *map_data)
+{
+	if (!map_data->walls.top_wall_v2 || !map_data->walls.block_wall_v2 || !map_data->walls.down_wall_v1 || !map_data->walls.down_wall_v2 || !map_data->walls.left_wall_v1)
+		failure(1);
+	if (!map_data->walls.left_wall_v2 || !map_data->walls.down_left_wall || !map_data->walls.down_right_wall || !map_data->walls.full_wall)
+		failure(1);
+	if (!map_data->walls.top_wall_v1 || !map_data->walls.right_wall_v2 || !map_data->walls.down_right_wall || !map_data->walls.right_wall_v1)
+		failure(1);
+	if (!map_data->componets.player || !map_data->componets.exit || !map_data->componets.collectible || !map_data->floors.floor)
+		failure(1);
 }
 
 void	init_images(t_data *map_data)
 {
 	init_walls(map_data);
-	init_floors(map_data);
 	init_componets(map_data);
-	// fprintf(stderr,"%p",map_data->image.amongus);
-	// if (!map_data->image.img_exit)
-	// 	failure(1);
-	// printf("%p\n", map_data->image.floor);
+	protections(map_data);
 }
